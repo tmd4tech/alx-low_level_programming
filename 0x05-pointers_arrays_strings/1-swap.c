e8e6c63882c2d1a2530f0c8a667e042e3b809023
tmd4tech <temidayoakanmu69@gmail.com>
2022-12-19 01:39:39 -0800

    task00

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,39 +1,21 @@
 #include "main.h"
 
-#include <stdio.h>
-
 
 
 /**
  *
- *  * main - check the code
+ *  * reset_to_98 - updates the value of the variable
  *
- *   *
+ *   * the pointer points to to 98
  *
- *    * Return: Always 0.
+ *    * @n: pointer to the variable to update
  *
  *     */
 
-int main(void)
+void reset_to_98(int *n)
 
 {
 
-	    int a;
-
-	        int b;
-
-
-
-		    a = 98;
-
-		        b = 42;
-
-			    printf("a=%d, b=%d\n", a, b);
-
-			        swap_int(&a, &b);
-
-				    printf("a=%d, b=%d\n", a, b);
-
-				        return (0);
+		*n = 98;
 
 }
